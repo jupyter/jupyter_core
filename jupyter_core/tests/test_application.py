9a91b23fc4d7a726c81d5994cea9064687593401
Min RK <benjaminrk@gmail.com>
2015-04-17 14:53:24 -0700

    exercise default values of JupyterApp's traits

--- jupyter_core/tests/test_application.py
+++ jupyter_core/tests/test_application.py
@@ -2,3 +2,8 @@ from jupyter_core import application
 
 def test_basic():
     app = application.JupyterApp()
+
+def test_default_traits():
+    app = application.JupyterApp()
+    for trait_name in app.traits():
+        value = getattr(app, trait_name)
