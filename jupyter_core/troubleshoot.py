d7a2728f42c38f1aa6709d4762db8f83abeafa52
Min RK <benjaminrk@gmail.com>
2016-03-07 12:03:21 +0100

    handle missing data in troubleshoot script

--- jupyter_core/troubleshoot.py
+++ jupyter_core/troubleshoot.py
@@ -24,7 +24,7 @@ def subs(cmd):
     try:
         stdout = subprocess.check_output(cmd)
         return stdout.decode('utf-8').strip()
-    except subprocess.CalledProcessError:
+    except (OSError, subprocess.CalledProcessError):
         return None
 
 
@@ -74,16 +74,20 @@ def main():
     print('\n' + 'platform.platform():')
     print('\t' + environment_data['platform'])
 
-    print('\n' + 'which -a jupyter:')
-    print('\t' + environment_data['which'])
+    if environment_data['which']:
+        print('\n' + 'which -a jupyter:')
+        for line in environment_data['which'].split('\n'):
+            print('\t' + line)
 
-    print('\n' + 'pip list:')
-    for package in environment_data['pip'].split('\n'):
-        print('\t' + package)
+    if environment_data['pip']:
+        print('\n' + 'pip list:')
+        for package in environment_data['pip'].split('\n'):
+            print('\t' + package)
 
-    print('\n' + 'conda list:')
-    for package in environment_data['conda'].split('\n'):
-        print('\t' + package)
+    if environment_data['conda']:
+        print('\n' + 'conda list:')
+        for package in environment_data['conda'].split('\n'):
+            print('\t' + package)
 
 
 if __name__ == '__main__':
