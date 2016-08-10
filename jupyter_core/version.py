c600d7e2d5240a02c3308d7cb4f2cfb0fa6c45c5
Min RK <benjaminrk@gmail.com>
2016-08-10 13:53:41 +0200

    back to dev

--- jupyter_core/version.py
+++ jupyter_core/version.py
@@ -1,6 +1,6 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (4, 1, 1)
+version_info = (4, 2, 0, 'dev')
 
 __version__ = '.'.join(map(str, version_info))
