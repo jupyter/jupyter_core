89e40551b146b584cd14dbae291a99b0a2886c4d
Thomas Gläßle <t_glaessle@gmx.de>
2019-05-02 19:04:35 +0200

    Prevent setup.py from performing installing

--- setup.py
+++ setup.py
@@ -3,7 +3,24 @@
 
 # Copyright (c) Juptyer Development Team.
 # Distributed under the terms of the Modified BSD License.
+import sys
 from setuptools import setup
+from setuptools.command.bdist_egg import bdist_egg
+
+
+class bdist_egg_disabled(bdist_egg):
+    """Disabled version of bdist_egg
+
+    Prevents setup.py install from performing setuptools' default easy_install,
+    which it should never ever do.
+    """
+    def run(self):
+        sys.exit("Aborting implicit building of eggs. Use `pip install .` to install from source.")
+
 
 # Loads metadata and options from setup.cfg:
-setup()
+setup(
+    cmdclass={
+        'bdist_egg': bdist_egg if 'bdist_egg' in sys.argv else bdist_egg_disabled,
+    },
+)
