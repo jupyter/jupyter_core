9aee793468b6c0bb13df4c1b57f8e4495e525984
Jason Grout <jgrout6@bloomberg.net>
2021-10-26 17:11:32 -0700

    4.9.0
    dist/jupyter_core-4.9.0-py3-none-any.whl
    md5: c46d9fe8ad9cb6c4832b68c550e7be21
    sha1: 72edaac8f2a77dc108eb6f85a7eb1fca4296a4d9
    sha256: d03ef9eaa5b28925d30c8f83547ccc24b95cd0394b272000a1a6ab069a652be3
    
    dist/jupyter_core-4.9.0.tar.gz
    md5: ea35be1408c9bd735dd75c9f3a8850bc
    sha1: fefe55442b0c100dc3f2d1915bffae58d1828cda
    sha256: adfc754536b34a1cd987c7991a24536d95f4a791e9761e9327fa919fc8d424f3

--- jupyter_core/version.py
+++ jupyter_core/version.py
@@ -11,7 +11,7 @@ VersionInfo = namedtuple('VersionInfo', [
     'serial'
 ])
 
-version_info = VersionInfo(4, 9, 0, 'candidate', 0)
+version_info = VersionInfo(4, 9, 0, 'final', 0)
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': '', 'dev': 'dev'}
 
