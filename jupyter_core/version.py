d3f61f336bfe5c6c63e1af0d8c9fb0dea59f8793
Steven Silvester <steven.silvester@ieee.org>
2022-07-08 05:33:35 -0500

    Release 4.11.1

--- jupyter_core/version.py
+++ jupyter_core/version.py
@@ -5,8 +5,8 @@ from collections import namedtuple
 
 VersionInfo = namedtuple("VersionInfo", ["major", "minor", "micro", "releaselevel", "serial"])
 
-version_info = VersionInfo(4, 11, 0, "final", 0)
-__version__ = "4.11.0"
+version_info = VersionInfo(4, 11, 1, "final", 0)
+__version__ = "4.11.1"
 
 _specifier_ = {"alpha": "a", "beta": "b", "candidate": "rc", "final": "", "dev": "dev"}
 
