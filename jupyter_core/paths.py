69919fa13028000ce09b7efd6746dc923dafcb36
Min RK <benjaminrk@gmail.com>
2022-12-14 10:57:25 +0100

    update comment about conda root env
    Co-authored-by: Jason Grout <jasongrout@users.noreply.github.com>

--- jupyter_core/paths.py
+++ jupyter_core/paths.py
@@ -78,7 +78,7 @@ def prefer_environment_over_user() -> bool:
     if sys.prefix != sys.base_prefix:
         return True
 
-    # If sys.prefix indicates Python comes from a conda/mamba environment, default to True
+    # If sys.prefix indicates Python comes from a conda/mamba environment that is not the root environment, default to True
     if (
         "CONDA_PREFIX" in os.environ
         and sys.prefix.startswith(os.environ["CONDA_PREFIX"])
