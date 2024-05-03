65fe66f7bb6771e9e2bce62f36e345f585f00368
Matthias Bussonnier <bussonniermatthias@gmail.com>
2024-05-03 10:08:58 +0200

    MAINT: use new syntax for intersphinx_mapping.
    The old syntax is deprected since sphinx 6.2 and will be removed in teh
    future.

--- docs/conf.py
+++ docs/conf.py
@@ -296,7 +296,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"https://docs.python.org/3/": None}
+intersphinx_mapping = {"python": ("https://docs.python.org/3/", None)}
 
 
 def setup(_):
