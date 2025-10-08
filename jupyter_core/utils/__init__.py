8d9753d733e43cb5d65adf65bdd5e71b1c78309c
M Bussonnier <bussonniermatthias@gmail.com>
2025-10-08 14:37:44 +0200

    Start to test on 3.14 and 3.14-free-threaded
    This starts testing on 3.14 and and 3.14t as 3.14
    introduce some eventloop policy deprecation we are
    not ready to deal with, we ignore warnings only on
    Python 3.14
    
    Some tornado tests are failing on all windows platform,
    we skip those.

--- jupyter_core/utils/__init__.py
+++ jupyter_core/utils/__init__.py
@@ -176,7 +176,17 @@ def ensure_event_loop(prefer_selector_loop: bool = False) -> asyncio.AbstractEve
         loop = asyncio.get_running_loop()
     except RuntimeError:
         if sys.platform == "win32" and prefer_selector_loop:
-            loop = asyncio.WindowsSelectorEventLoopPolicy().new_event_loop()
+            if (3, 14) <= sys.version_info < (3, 15):
+                # ignore deprecation only for 3.14 and revisit later.
+                with warnings.catch_warnings():
+                    warnings.filterwarnings(
+                        "ignore",
+                        DeprecationWarning,
+                        message=".*WindowsSelectorEventLoopPolicy.*",
+                    )
+                    loop = asyncio.WindowsSelectorEventLoopPolicy().new_event_loop()
+            else:
+                loop = asyncio.WindowsSelectorEventLoopPolicy().new_event_loop()
         else:
             loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
